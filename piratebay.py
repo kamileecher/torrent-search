ae26b7a111a9932f1688f66e1bdb4a2bb2a497b7
kamileecher <58888200+kamileecher@users.noreply.github.com>
2021-05-17 19:46:53 +0300

    Update piratebay.py

--- piratebay.py
+++ piratebay.py
@@ -8,18 +8,18 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 app = Client("trntsrcbot", api_id=int(os.environ.get("API_ID")), api_hash=os.environ.get("API_HASH"), bot_token=os.environ.get("BOT_TOKEN"))
 
 
-print("\nBot Started\n")
+print("\nBot Baslatıldı\n")
 
 
 @app.on_message(filters.command(['start']))
 async def start(_, message):
-    await message.reply_text("Hello /help \n Bot by @kamileecher")
+    await message.reply_text("Merhaba Bilgi için /help komutuna tıkla\n@kamileecher")
 
 
 
 @app.on_message(filters.command(['help']))
 async def help(_, message):
-    await message.reply_text("Exp.: /find titanic")
+    await message.reply_text("Ornek: /find naruto\nOrnek: /find apps\nOrnek: /find titanic\nOrnek: /find adobe")
 
 m = None
 i = 0
@@ -38,35 +38,35 @@ async def find(_, message):
     except:
         pass
     if len(message.command) < 2:
-        await message.reply_text("Kullanım: /find query")
+        await message.reply_text("Kullanım: /find aranacak şey")
         return
     query = message.text.split(None, 1)[1].replace(" ", "%20")
-    m = await message.reply_text("Searching")
+    m = await message.reply_text("Aranıyor...")
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(f"https://api.api-zero.workers.dev/piratebay/{query}") \
                     as resp:
                 a = json.loads(await resp.text())
     except:
-        await m.edit("Found Nothing.")
+        await m.edit("Hiçbir şey bulunamadı.")
         return
     result = (
-        f"**📚Page - {i+1}**\n\n"
-        f"✔️Name: {a[i]['Name']}\n"
+        f"**📚Sayfa - {i+1}**\n\n"
+        f"✔️Adı: {a[i]['Name']}\n"
         f"✔️{a[i]['Uploader']} on "
         f"{a[i]['Date']}\n" 
-        f"✔️Size: {a[i]['Size']}\n"
+        f"✔️Boyut: {a[i]['Size']}\n"
         f"✔️Leechers: {a[i]['Leechers']} || "
         f"✔️Seeders: {a[i]['Seeders']}\n"
-        f"✔️Type: {a[i]['Category']}\n"
-        f"✔️Magnet: `{a[i]['Magnet']}`\n\n\n"
+        f"✔️Tipi: {a[i]['Category']}\n"
+        f"✔️Torrent: `{a[i]['Magnet']}`\n\n\n"
     )
     await m.edit(
         result,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(f"Next",
+                    InlineKeyboardButton(f"Sonra",
                                          callback_data="next"),
                     InlineKeyboardButton(f"{emoji.CROSS_MARK}",
                                          callback_data="delete")
@@ -85,26 +85,26 @@ async def callback_query_next(_, message):
     global query
     i += 1
     result = (
-        f"**📚Page - {i+1}**\n\n"
-        f"✔️Name: {a[i]['Name']}\n"
+        f"**📚Sayfa - {i+1}**\n\n"
+        f"✔️Adı: {a[i]['Name']}\n"
         f"✔️{a[i]['Uploader']} on "
         f"{a[i]['Date']}\n" 
-        f"✔️Size: {a[i]['Size']}\n"
+        f"✔️Boyut: {a[i]['Size']}\n"
         f"✔️Leechers: {a[i]['Leechers']} || "
         f"✔️Seeders: {a[i]['Seeders']}\n"
-        f"✔️Type: {a[i]['Category']}\n"
-        f"✔️Magnet: `{a[i]['Magnet']}`\n\n\n"
+        f"✔️Tipi: {a[i]['Category']}\n"
+        f"✔️Torrent: `{a[i]['Magnet']}`\n\n\n"
     )
     await m.edit(
         result,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(f"Prev",
+                    InlineKeyboardButton(f"Önce",
                                          callback_data="previous"),
                     InlineKeyboardButton(f"{emoji.CROSS_MARK}",
                                          callback_data="delete"),
-                    InlineKeyboardButton(f"Next",
+                    InlineKeyboardButton(f"Sonra",
                                          callback_data="next")
                     
                 ]
@@ -122,26 +122,26 @@ async def callback_query_previous(_, message):
     global query
     i -= 1
     result = (
-        f"**📚Page - {i+1}**\n\n"
-        f"✔️Name: {a[i]['Name']}\n"
+        f"**📚Sayfa - {i+1}**\n\n"
+        f"✔️Adı: {a[i]['Name']}\n"
         f"✔️{a[i]['Uploader']} on "
         f"{a[i]['Date']}\n" 
-        f"✔️Size: {a[i]['Size']}\n"
+        f"✔️Boyut: {a[i]['Size']}\n"
         f"✔️Leechers: {a[i]['Leechers']} || "
         f"✔️Seeders: {a[i]['Seeders']}\n"
-        f"✔️Type: {a[i]['Category']}\n"
-        f"✔️Magnet: `{a[i]['Magnet']}`\n\n\n"
+        f"✔️Tipi: {a[i]['Category']}\n"
+        f"✔️Torrent: `{a[i]['Magnet']}`\n\n\n"
     )
     await m.edit(
         result,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(f"Prev",
+                    InlineKeyboardButton(f"Önce",
                                          callback_data="previous"),
                     InlineKeyboardButton(f"{emoji.CROSS_MARK}",
                                          callback_data="delete"),
-                    InlineKeyboardButton(f"Next",
+                    InlineKeyboardButton(f"Sonra",
                                          callback_data="next")
                 ]
             ]
